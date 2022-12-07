ce2d0330dfd1665b6d9e34f3100ef78bed687b83
ymyt <hope09476@gmail.com>
2022-12-07 11:28:21 +0600

    register + login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+from .serializers import RegisterUserSerializer
+
+
+
+class RegisterUserView(APIView):
+    @swagger_auto_schema(request_body=RegisterUserSerializer())
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("Вы успешно зарегистрировались", status=201)
+
