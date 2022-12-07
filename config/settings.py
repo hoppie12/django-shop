c1cd2e659c856b006faa78a78936dff5cb25c2c7
ymyt <hope09476@gmail.com>
2022-12-07 10:24:16 +0600

    create model

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.User'
 
 
 
