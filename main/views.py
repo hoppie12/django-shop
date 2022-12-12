36a147b3ec9af66eaff9370294d6e94a03655481
ymyt <hope09476@gmail.com>
2022-12-12 10:24:05 +0600

    models comment + rating

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
 
@@ -10,3 +13,21 @@ class CategoryViewSet(ModelViewSet):
 class ProductViewSet(ModelViewSet):
     queryset  = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ['retrieve', 'list']:
+            #если это запрос на листинг или детализацию
+            return [] # разрешаем всем
+        return [IsAdminUser()] # разрешаем только админам
+
+    @action(['GET'], detail=False)
+    def search(self, request):
+        q = request.query_params.get('q')
+        # get_gueryset - Product.objects.all()
+        # get_serializer - ProductSerializer
+        queryset = Product.objects.filter(title__icontains=q) #title ilike '%hello%'
+        serializer= self.get_serializer(queryset, many=True)
+        return  Response(serializer.data, status=200)
+
+    
